869c7026eeaec99f8fd837fdfaf83ce1fd140693
vcjronijohn <ronijohnveronica@gmail.com>
2020-02-20 16:00:42 -0500

    added homework

---
+++ timer.js
@@ -0,0 +1,52 @@
+
+
+// function myTimer(){
+//     document.getElementById("timer").innerHTML = input;
+//     let input = parseInt(document.getElementById("userInput").value);
+//     while(!isNaN(input) && input > 0){
+//         document.getElementById("timer").innerHTML = input;
+//         input--;
+//     }
+// }
+
+function myTimer(){
+    let input = parseInt(document.getElementById("userInput").value);
+    let clock = setInterval(function(){
+        document.getElementById("timer").innerHTML = input;
+        input--; 
+    }, 1000);
+}
+
+
+
+
+
+
+
+
+
+
+// function myTimer(){
+//     let input = parseInt(document.getElementById("userInput").value);
+//     let int = setInterval(function() {
+//         while(!isNaN(input) && input > 0){
+//             document.getElementById("timer").innerHTML = input;
+//             input--;
+            
+//         }}, 1000);
+//     }
+
+
+
+
+
+
+
+// function myTimer(){
+//     let input = parseInt(document.getElementById("userInput"));
+//     var int = setInterval(function(){
+//         document.getElementById("timer").innerHTML = input;
+//         input-- || clearInterval(int);
+//     }, 1000);
+// }
+
